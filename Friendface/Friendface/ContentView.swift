a10a822fe0d9a834fc335006503ded3091bff9bf
galih <samudragalih94@gmail.com>
2024-09-11 10:18:41 +0700

    wip initial project

---
+++ Friendface/Friendface/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Friendface
+//
+//  Created by Galih Samudra on 11/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Friendface/Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Galih Samudra on 11/09/24.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
